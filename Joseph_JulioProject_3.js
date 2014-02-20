1f2b071abb82e95a7c3a63e41e81c24ceb5896f4
Julio <Juliojoseph718@yahoo.com>
2014-02-20 07:29:00 -0500

    first commit

---
+++ Joseph_JulioProject_3.js
@@ -0,0 +1,33 @@
+//alert("JavaScript works!");
+ 
+ 	// Global Variables 
+ var workToday = true;
+ var hrsPerWeek = 40;
+ var companyName = "NVC Logistics";
+ var coWorkers = ["Sergio", "Barbra", "Sal"]
+ 
+ 	
+ 	
+ 	
+ 	//functions 
+ var workWith = function(myCoWorker){
+   	if (coWorkers === "Barbra") {
+   	console.log("I love riding with" + myCoWorker)
+   	
+   };
+   
+   
+ var hrsPerDay = function(){
+ 
+ }
+ 
+ 
+ var 002
+
+
+ 
+ 
+ 
+ 	// Main code 
+ workWith(coWorkers[2]);
+
