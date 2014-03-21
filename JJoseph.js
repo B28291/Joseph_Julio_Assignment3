27528f97343195dc495988c8fe02d8d88d13fb0b
Julio <Juliojoseph718@yahoo.com>
2014-03-20 21:33:16 -0400

    change

---
+++ JJoseph.js
@@ -0,0 +1,61 @@
+//alert("JavaScript works!");
+
+
+	//global variables
+ 
+var workToday = false;
+var companyName = "IceCoffee";
+var excerices = ["chest","legs","arms","back"];
+
+
+	//functions
+	
+var mornings = function(myDay){
+ 	if (myDay == true){
+ 	 	console.log("I have to work today.")
+ 	}else{console.log("Ill go to the library or the gym.")};
+
+};	
+
+var startOff = function(){
+	
+		
+		
+	
+
+
+};
+
+var workOut = function(){
+
+
+
+}; 
+
+var dinner = function(){
+
+
+
+};
+
+var myDay = function(){
+
+
+};
+
+
+
+
+
+
+
+
+
+
+	//main code 
+	
+morning(workToday);
+startOff();
+workOut();
+dinner():
+myDay();
