d6bf777be5e85a9177d5e57518f60305221b7bb9
Julio <Juliojoseph718@yahoo.com>
2014-03-20 23:15:52 -0400

    second commit

--- JJoseph.js
+++ JJoseph.js
@@ -6,8 +6,9 @@
 var workToday = false;
 var companyName = "IceCoffee";
 var excerices = ["chest","legs","arms","back"];
-
-
+var dailyHobbies = ["Exercise","babysitt","library",];
+var commute = 1;
+	
 	//functions
 	
 var mornings = function(myDay){
@@ -17,14 +18,21 @@ var mornings = function(myDay){
 
 };	
 
-var startOff = function(){
-	
-		
-		
-	
+var startOff = function(myDay){};
 
-
+for( i = 0;i < dailyHobbies.length; i++){}
+		if(dailyHobbies[i] == ("Exercise"){
+			for(p = 0; p < excerices; p++){
+ 		 	console.log("Ill start off my day with " + dailyHobbies[1] + "and ill do " + excerices[P]);
+			};
+		
+		}else{console.log("Ill have to " + dailyHobbies[2]);
+	
 };
+
+	
+	 
+
 
 var workOut = function(){
 
@@ -55,7 +63,8 @@ var myDay = function(){
 	//main code 
 	
 morning(workToday);
-startOff();
+var doingToday = prompt("What are you doing today?");
+startOff(doingToday);
 workOut();
 dinner():
 myDay();
